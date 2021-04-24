d8e2abafd26fbbf2a91be5a1d09c3c83f7cfdd74
Ilya Leykin <imleykin@gmail.com>
2021-04-24 16:57:40 +0500

    add createTextElement

--- re-react.js
+++ re-react.js
@@ -3,15 +3,20 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children: children.map(child =>
-        typeof child === "object"
-          ? child
-          : createTextElement(child)
-      ),
+      children: children.map(child => typeof child === "object" ? child : createTextElement(child)),
     },
   }
 }
 
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
 const ReReact = {
   createElement,
 }
